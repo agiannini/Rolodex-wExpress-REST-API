876306231c1e81c9e78a61a857ffe373f21fefe3
agiannini <alexanderegiannini@gmail.com>
2019-06-10 23:51:35 -0400

    made navbar collapsable

--- client/dist/polyfills.js
+++ client/dist/polyfills.js
@@ -5784,8 +5784,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\Users\Alex\Desktop\public_repo\Rolodex REST App with MEAN stack\client\src\polyfills.ts */"./src/polyfills.ts");
-module.exports = __webpack_require__(/*! C:\Users\Alex\Desktop\public_repo\Rolodex REST App with MEAN stack\client\node_modules\@angular-devkit\build-angular\src\angular-cli-files\models\jit-polyfills.js */"./node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/jit-polyfills.js");
+__webpack_require__(/*! C:\Users\Alex\Desktop\Rolodex-wExpress-REST-API\client\src\polyfills.ts */"./src/polyfills.ts");
+module.exports = __webpack_require__(/*! C:\Users\Alex\Desktop\Rolodex-wExpress-REST-API\client\node_modules\@angular-devkit\build-angular\src\angular-cli-files\models\jit-polyfills.js */"./node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/jit-polyfills.js");
 
 
 /***/ })
